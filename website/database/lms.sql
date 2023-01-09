6aaccdcd9a8d19f6404a96ae5b4ef46f34c92f3a
Judywen Guapin <37282871+jagwarthegreat@users.noreply.github.com>
2023-01-09 11:08:01 +0800

    update

--- website/database/lms.sql
+++ website/database/lms.sql
@@ -38,9 +38,9 @@ CREATE TABLE IF NOT EXISTS `tbl_books` (
 
 -- Dumping data for table lms.tbl_books: ~3 rows (approximately)
 INSERT INTO `tbl_books` (`book_id`, `book_title`, `book_author`, `book_publisher_name`, `book_category_id`, `book_isbn`, `book_year_published`, `book_qty`, `book_price`, `book_cover_img`, `book_location`, `date_added`) VALUES
-	(10, 'adsas', 'dasd', 'asd', 1, '123123', '2023', '100', '1500', '/static/uploads/cover_images/HOME_DECOR_INDOOR_WALL_WATER_FALL_IDEAS___WATER_FEATURES_DESIGN_IDEAS___WALL_MOUNTED_IDEAS_water_a.jpg', 'test loc', '2023-01-05 16:16:08'),
-	(11, '3', '3', '3', 2, '12123', '2021', '100', '1500', '/static/uploads/cover_images/320399216_527773672707625_652718725034596717_n.jpg', 'test', '2023-01-05 16:27:04'),
-	(12, '3', '3', '3', 4, '123123', '2022', '50', '1500', '', 'test', '2023-01-05 16:27:41');
+	(10, 'Book 1', 'dasd', 'asd', 1, '123123', '2023', '100', '1500', '/static/uploads/cover_images/HOME_DECOR_INDOOR_WALL_WATER_FALL_IDEAS___WATER_FEATURES_DESIGN_IDEAS___WALL_MOUNTED_IDEAS_water_a.jpg', 'test loc', '2023-01-05 16:16:08'),
+	(11, 'Book 2', '3', '3', 2, '12123', '2021', '100', '1500', '/static/uploads/cover_images/320399216_527773672707625_652718725034596717_n.jpg', 'test', '2023-01-05 16:27:04'),
+	(12, 'Book 3', '3', '3', 4, '123123', '2022', '50', '1500', '', 'test', '2023-01-05 16:27:41');
 
 -- Dumping structure for table lms.tbl_book_category
 CREATE TABLE IF NOT EXISTS `tbl_book_category` (
@@ -59,6 +59,35 @@ INSERT INTO `tbl_book_category` (`book_category_id`, `book_category`, `book_cate
 	(4, 'Thriller', NULL, '2023-01-04 15:29:47'),
 	(5, 'Sci-Fi', 'test', '2023-01-04 15:47:36');
 
+-- Dumping structure for table lms.tbl_borrowed_books
+CREATE TABLE IF NOT EXISTS `tbl_borrowed_books` (
+  `borrow_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `status` varchar(1) NOT NULL DEFAULT 'S' COMMENT 'B=borrowed, R=returned, S=saved',
+  `date_borrowed` datetime DEFAULT NULL,
+  `date_added` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`borrow_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- Dumping data for table lms.tbl_borrowed_books: ~2 rows (approximately)
+INSERT INTO `tbl_borrowed_books` (`borrow_id`, `user_id`, `status`, `date_borrowed`, `date_added`) VALUES
+	(1, 1, 'S', '2023-01-09 00:00:00', '2023-01-09 10:11:46'),
+	(2, 1, 'S', '2023-01-09 00:00:00', '2023-01-09 10:24:50');
+
+-- Dumping structure for table lms.tbl_borrowed_books_details
+CREATE TABLE IF NOT EXISTS `tbl_borrowed_books_details` (
+  `borrow_detail_id` int(11) NOT NULL AUTO_INCREMENT,
+  `borrow_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `qty` varchar(10) DEFAULT NULL,
+  `qty_returned` varchar(10) DEFAULT NULL,
+  `date_returned` datetime NOT NULL,
+  `date_added` datetime NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`borrow_detail_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table lms.tbl_borrowed_books_details: ~0 rows (approximately)
+
 -- Dumping structure for table lms.tbl_user
 CREATE TABLE IF NOT EXISTS `tbl_user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -67,17 +96,18 @@ CREATE TABLE IF NOT EXISTS `tbl_user` (
   `lname` varchar(150) NOT NULL,
   `username` varchar(150) NOT NULL,
   `password` varchar(150) NOT NULL,
+  `category` varchar(150) NOT NULL,
   `date_added` datetime DEFAULT current_timestamp(),
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table lms.tbl_user: ~4 rows (approximately)
-INSERT INTO `tbl_user` (`id`, `fname`, `mname`, `lname`, `username`, `password`, `date_added`) VALUES
-	(1, 'Eduard RIno', 'Questo', 'Carton', 'jag', 'sha256$srhW5avnCjLm7Tkj$0502c352f2f30fa5dfb8111d4c3e72b7c13d9f3da7f76bb3d72aabd5ba97977f', '2023-01-03 15:56:07'),
-	(3, 'Jagwarthegreat', 'Questo', 'Carton', 'rin', 'sha256$srhW5avnCjLm7Tkj$0502c352f2f30fa5dfb8111d4c3e72b7c13d9f3da7f76bb3d72aabd5ba97977f', '2022-11-25 16:29:56'),
-	(4, 'Kaye', 'N', 'Jacildo', 'k', 'sha256$M3gleJORbojiiJZa$9fa9bf27c354e5e782e95065b1619c9a055e431f345e96c32871fc96407eac75', '2022-11-28 14:22:45'),
-	(5, 'jep', 'jep', 'jep', 'jep', 'sha256$B9R8kNibhLTUMID7$01913bf18355117e4fe44cb46088414b749b920628da446028d430998943d565', '2022-11-28 21:12:53');
+INSERT INTO `tbl_user` (`id`, `fname`, `mname`, `lname`, `username`, `password`, `category`, `date_added`) VALUES
+	(1, 'Eduard RIno', 'Questo', 'Carton', 'jag', 'sha256$srhW5avnCjLm7Tkj$0502c352f2f30fa5dfb8111d4c3e72b7c13d9f3da7f76bb3d72aabd5ba97977f', '', '2023-01-03 15:56:07'),
+	(3, 'Jagwarthegreat', 'Questo', 'Carton', 'rin', 'sha256$srhW5avnCjLm7Tkj$0502c352f2f30fa5dfb8111d4c3e72b7c13d9f3da7f76bb3d72aabd5ba97977f', '', '2022-11-25 16:29:56'),
+	(4, 'Kaye', 'N', 'Jacildo', 'k', 'sha256$M3gleJORbojiiJZa$9fa9bf27c354e5e782e95065b1619c9a055e431f345e96c32871fc96407eac75', '', '2022-11-28 14:22:45'),
+	(5, 'jep', 'jep', 'jep', 'jep', 'sha256$B9R8kNibhLTUMID7$01913bf18355117e4fe44cb46088414b749b920628da446028d430998943d565', '', '2022-11-28 21:12:53');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
